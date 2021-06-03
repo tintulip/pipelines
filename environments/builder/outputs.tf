bb3f5a50eaa39a11d94b5973b446a69ba0fdf132
Yusra Dahir (TIN TULIP) <yusra.dahir+tintulip@thoughtworks.com>
2021-06-03 11:44:49 +0100

    #66 remove access key and encrypted secret key outputs.
    Co-Authored-By: Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>
    Co-authored-by: Meissane Chami <meissane.chami+tintulip@thoughtworks.com>

--- environments/builder/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "encrypted_github_actions_secret_key" {
-  value = module.codepipeline_user.encrypted_secret_key
-}
-
-output "github_actions_access_key" {
-  value = module.codepipeline_user.access_key
-}
