ce9add183280c2c15b32b7ce8fec33d37c54d52c
Yusra Dahir (TIN TULIP) <yusra.dahir+tintulip@thoughtworks.com>
2021-06-07 15:20:07 +0100

    #56 remove the bucket policy for the s3 bucket as we had same error.
    Co-Authored-By: Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>

--- environments/builder/pipeline_artifacts.tf
+++ environments/builder/pipeline_artifacts.tf
@@ -2,23 +2,3 @@ resource "aws_s3_bucket" "codepipeline_bucket" {
   bucket = "cla-pipeline-artifacts"
   acl    = "private"
 }
-
-resource "aws_s3_bucket_policy" "codepipeline_bucket" {
-  bucket = aws_s3_bucket.codepipeline_bucket.id
-  policy = data.aws_iam_policy_document.codepipeline_bucket_policy.json
-}
-
-data "aws_iam_policy_document" "codepipeline_bucket_policy" {
-  statement {
-    actions = ["s3:GetObject"]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::961889248176:role/app_deployer"]
-    }
-
-    resources = [
-      "arn:aws:s3:::cla-pipeline-artifacts/*"
-    ]
-  }
-}
